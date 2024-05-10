2c134c2f6fe77ca16c6bfb84164bd593660db1f4
Burak Özdemir <ozdemrburak@yahoo.com>
2024-05-10 14:09:29 +0300

    added scraping of product images
    scraping links of the product images

--- migros_data/data_scraping_product_names.py
+++ migros_data/data_scraping_product_names.py
@@ -6,7 +6,8 @@ import init_driver as id
 
 
 def data_scraping_product_names(url_list):
-    product_names_dict = {"product_name": []}
+    product_names_dict = {"product_name": [],
+                          "product_image": []}
     display = 0
     for url in url_list:
         print(display)
@@ -14,10 +15,12 @@ def data_scraping_product_names(url_list):
         driver = id.init_driver(url)
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h3')))
         html_part = driver.execute_script(
-            'var element = document.querySelector("body > sm-root > div > main > sm-product > article > sm-product-detail-page > div.product-detail-page.ng-star-inserted > div.product-detail-wrapper > div > h3"); if (element) {return element.outerHTML;} else {return null;} ')
+            'var element =document.querySelector("body > sm-root > div > main > sm-product > article > sm-product-detail-page > div.product-detail-page.ng-star-inserted > div.product-detail-wrapper"); if (element) {return element.outerHTML;} else {return null;} ')
         soup = BeautifulSoup(html_part, "html.parser")
+        product_image = soup.find('img').get('src')
         product_name = soup.find('h3').text
         product_names_dict["product_name"].append(product_name)
+        product_names_dict["product_image"].append(product_image)
         driver.quit()
 
         display += 1
--- migros_data/shaping_json.py
+++ migros_data/shaping_json.py
@@ -11,6 +11,7 @@ def shaping_json(data):
 
                 "product_id": "",
                 "product_name": "",
+                "product_image": "",
                 "business_registration_number": "",
                 "net_amount": "",
                 "ingredients": [],
@@ -20,6 +21,7 @@ def shaping_json(data):
 
         temp_dict["product_id"] = i
         temp_dict["product_name"] = data["product_name"][i]
+        temp_dict["product_image"] = data["product_image"][i]
         temp_dict["business_registration_number"] = data["İşletme Kayıt No"][i]
         temp_dict["net_amount"] = data["Net Miktar (g/ml)"][i]
         temp_dict["ingredients"] = data["İçindekiler"][i]
